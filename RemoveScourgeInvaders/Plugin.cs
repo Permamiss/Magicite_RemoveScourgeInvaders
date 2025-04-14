5c4fd9e9387ebf8466a6336fcf9d9f3de22309dd
Permamiss <39325797+Permamiss@users.noreply.github.com>
2025-04-14 18:43:29 -0400

    Initial commit

---
+++ RemoveScourgeInvaders/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.Emit;
+using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+
+namespace RemoveScourgeInvaders
+{
+	[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+	[BepInProcess("Magicite.exe")]
+	public class RemoveInvader : BaseUnityPlugin
+	{
+		public void Awake()
+		{
+			Harmony.CreateAndPatchAll(typeof(RemoveInvader), "RemoveInvader");
+		}
+
+		[HarmonyPatch(typeof(GameScript), "Invader")]
+		[HarmonyTranspiler]
+		private static IEnumerable<CodeInstruction> RemoveInvaderTranspiler(IEnumerable<CodeInstruction> instructions)
+		{
+			CodeMatcher codeMatcher = new CodeMatcher(instructions);
+
+			codeMatcher.MatchForward
+			(
+				false,
+				new CodeMatch(OpCodes.Ldarg_0)
+			).RemoveInstruction().Insert(new CodeInstruction(OpCodes.Ldnull));
+
+			return codeMatcher.Instructions();
+		}
+	}
+}
---
+++ RemoveScourgeInvaders/obj/Debug/net35/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace RemoveScourgeInvaders
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "RemoveScourgeInvaders";
+        public const string PLUGIN_NAME = "Remove Scourge Invaders";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}

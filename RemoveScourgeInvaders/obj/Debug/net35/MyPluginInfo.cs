97e41a7a6d947aa53a9a2ef12db9f8631f20a6ad
Permamiss <39325797+Permamiss@users.noreply.github.com>
2025-04-22 21:39:11 -0400

    Delete RemoveScourgeInvaders/obj directory

--- RemoveScourgeInvaders/obj/Debug/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RemoveScourgeInvaders
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "RemoveScourgeInvaders";
-        public const string PLUGIN_NAME = "Remove Scourge Invaders";
-        public const string PLUGIN_VERSION = "1.1.0";
-    }
-}
